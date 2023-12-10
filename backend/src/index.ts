829b1439b14c74a859147ba184064bee1dae56e5
Alexander <100996481+aleolik@users.noreply.github.com>
2023-12-10 14:50:53 +0100

    Update index.ts
    wait for database to start , then run server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -11,26 +11,21 @@ import initalizeWebSocketServer from '../src/WSS/wss_init'
 import cookieParser from 'cookie-parser'
 
 const app = express()
-// use cors
 const corsOptions = {
     origin: 'http://localhost:3000',
     credentials: true, // Allow sending cookies with the request
 }
 app.use(cors(corsOptions))
-// cookie-parser
 app.use(cookieParser())
-// use json
 app.use(express.json())
-// use global router
-//
 app.use('/api',globalRouter)
-// use error handling
+// error handling middleware must be the last included middleware in middlewares
 app.use(ErrorHandlingMiddleware)
 initalizeWebSocketServer(app)
 
 const start = async() => {
 
-    startDB()
+    await startDB()
 
     const PORT = parseInt(process.env.PORT)
 
